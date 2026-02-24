d0e42ac3beff3fec46bac6669ab7b2cb2345c3d4
Alan Acosta Jacinto <acostajacinto805@gmail.com>
2026-02-24 10:00:14 -0600

    Subiendo estilos del index

--- cifrado_playfair/index.php
+++ cifrado_playfair/index.php
@@ -5,27 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cifrado-Playfair</title>
+    <link href="https://fonts.googleapis.com/css2?family=Krub:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="preload" href="css/Styles.css" as="style"> <!--Se utiliza lo siguiente para mayor performas, es mas rapido y de buena practica-->
+    <link rel="stylesheet" href="css/Styles.css"> <!--Enlazamos la hoja de stilos con la etiqueta link-->
 </head>
 
 <body>
-    <form action="cifrado_playfair.php" method="GET">
-        <div>
-            <label>Palabra</label>
-            <input type="text" name="palabra">
-        </div>
-        <div>
-            <label>Llave</label>
-            <input type="text" name="llave">
-        </div>
+    <section > 
+        <h2>Cibrado Playfair </h2> 
+        <form action="cifrado_playfair.php" method="GET"  class="formulario">
+            <fieldset>
+                <legend> Reglas: </legend> 
+                <legend> La letra ñ y j no son consideradas </legend> 
+                <legend> si la palabra o llave tiene la letra j es cambiada por una i </legend>
+                <div class="campo">
+                    <label>Palabra</label>
+                    <input type="text" name="palabra">
+                </div>
+                <div class="campo">
+                    <label>Llave</label>
+                    <input type="text" name="llave">
+                </div>
 
-
-        <button type="submit">Cifrar</button>
-    </form>
-    <div>
-    <p><strong>Reglas:</strong></p>
-    <p>La letra ñ y j no son consideradas</p>
-    <p>si la palabra o llave tiene la letra j es cambiada por una i</p>
-    </div>
+                    <button type="submit" class ="boton">Cifrar</button>
+            </fieldset>
+        </form>
+    </section>    
 </body>
 
 </html>
